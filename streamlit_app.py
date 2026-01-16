e732eff95642dd883510d464b171e379aa47c7ed
rishitha013 <157488110+rishitha013@users.noreply.github.com>
2026-01-16 14:27:59 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,6 +50,6 @@ if ingredients_list:
         st.success(
             f'✅ Your Smoothie is ordered, {name_on_order}!'
         )
-st.write("🥝 SmoothieFroot API Response")
+
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+st.text(smoothiefroot_response.json())
